72f1d8ce61d03702e155fb765f08aa7174af2592
Huiqiang Huang <bruce0750@gmail.com>
2024-08-10 12:06:56 -0700

    minor fix

--- testing/bootstrap.js
+++ testing/bootstrap.js
@@ -1,4 +1,4 @@
 const chai = require('chai');
 const sinon = require('sinon');
-Object.assign(global, chai);
-global.sandbox = sinon.createSandbox();
+Object.assign(global, chai, { sandbox: sinon.createSandbox() });
+// global.sandbox = sinon.createSandbox();
