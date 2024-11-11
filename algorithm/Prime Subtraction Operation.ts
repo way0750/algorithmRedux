dc8369bfab62cdf6a0b3beef3ad744e89c44ea72
Huiqiang Huang <bruce0750@gmail.com>
2024-11-11 14:04:43 -0800

    done

--- algorithm/Prime Subtraction Operation.ts
+++ algorithm/Prime Subtraction Operation.ts
@@ -1,9 +1,5 @@
 /**
  * 2601. Prime Subtraction Operation
-Medium
-Topics
-Companies
-Hint
 You are given a 0-indexed integer array nums of length n.
 
 You can perform the following operation as many times as you want:
@@ -12,8 +8,6 @@ Pick an index i that you haven’t picked before, and pick a prime p strictly le
 Return true if you can make nums a strictly increasing array using the above operation and false otherwise.
 
 A strictly increasing array is an array whose each element is strictly greater than its preceding element.
-
- 
 
 Example 1:
 
@@ -39,4 +33,47 @@ Constraints:
 1 <= nums.length <= 1000
 1 <= nums[i] <= 1000
 nums.length == n
+
+4 4 6 10
+3 4 6 10
+
+5 1 3
+
+go from right to left
+    make sure that the current number is smaller then the one one right
+    if not then:
+        get prime number from 1...current
+            and current number - prime < number on right
+            if can't find it, return false
+default return turn
+
+the trick is to find prime numbers
+since constraint states that nums[i] < 1000
+so just pre-calculate all the primes before hand:
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009];
+
+time: n for going through the entire nums, prime is 169 so O(1)
+    O(n * 169) = O(n)
+space: prime is 169 so O(n)
  */
+
+const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009];
+
+var primeSubOperation = function(nums) {
+    for (let i = nums.length-2; i > -1; i--) {
+        const num = nums[i];
+        const rightNum = nums[i+1]
+        if (num >= rightNum) {
+            let pi = 0;
+            while (primes[pi] < num && num - primes[pi] >= rightNum) {
+                pi++;
+            }
+            if (primes[pi] >= num) {
+                return false;
+            } else {
+                nums[i] = num - primes[pi];
+            }
+        }
+    }
+    return true;
+};
