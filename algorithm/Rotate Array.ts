ac46ec9ef4a1566cf686ca8990fb3189915acadd
Huiqiang Huang <bruce0750@gmail.com>
2024-11-16 21:09:16 -0800

    add new solution

--- algorithm/Rotate Array.ts
+++ algorithm/Rotate Array.ts
@@ -31,58 +31,29 @@ Follow up:
 
 Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
 Could you do it in-place with O(1) extra space?
+
 k = 3
-
-f = length - k - 1 = 3
-b = length - 1
-f
-5,6,7,1,2,3,4
-b
-
-f
-1,4,5,2,3
-    b
+k = k % nums.length;
+if (k===0) return;
 
 k = 2
-k = 5
+a
+0,1,2,3,4
+  b
 
-length = 6
-should be 2 chunks
-6/5, ceil = 2
-f
-2,3,4,5,6,1
-  bf
+1,2,3,4,5
 
-
-k = 2
-      f
-[-1,-100,3,99]
-            b
-
- f
-[3,99,-1,-100]
-    b
-
-
-
-time: (nums)
-space: 1
-
-const chunkCount = Math.ceil(nums.length/k)-1;
-
-k = 4
-  f
-5,6,3,4,1,2
-          b
 */
-
 function rotate(nums: number[], k: number): void {
-    let back = nums.length-1;
-    for (let chunk = 0; chunk < Math.ceil(nums.length/k); chunk++) {
+    k = k % nums.length;
+    if (k===0) return;
+    let right = nums.length - k;
+    let left = Math.max(0, right - k);
+    while (right >= 0) {
         for (let i = 0; i < k; i++) {
-            let front = Math.max(0, back-k);
-            [nums[front], nums[back]] = [nums[back], nums[front]];
-            back--;
+            [nums[left + i], nums[right + i]] = [nums[right + i], nums[left + i]];
         }
+        left = Math.max(0, left - k);
+        right = right - k
     }
 };
