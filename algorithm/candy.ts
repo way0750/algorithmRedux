ff0457e9cd048045193b300c3a075feb2a495d0e
Huiqiang Huang <bruce0750@gmail.com>
2024-11-22 18:08:58 -0800

    solution

--- algorithm/candy.ts
+++ algorithm/candy.ts
@@ -27,4 +27,92 @@ Constraints:
 n == ratings.length
 1 <= n <= 2 * 104
 0 <= ratings[i] <= 2 * 104
+
  */
+
+/**
+
+1,2,3,4,5
+1,2,3,4,5
+
+1,2,2,3,2,4
+1,2,1,2,1,2
+
+4,3,2,1
+0,0,0,0
+3,2,1,0
+3,2,1,0
+1,1,1,1
+4,3,2,1
+
+1,2,3,4
+1,2,3,4
+go from left to right
+compare current number ONLY to the left number
+if larger, take left number's candy count and + 1
+    else set canday count to 0
+
+from right to left
+    if current number is larger than the one on the right
+        if current candy count is already larger
+            do nothing
+        if smaller, add 1
+
+1,0,2
+0,0,1
+1,0,0
+1,0,1
+1,1,1
+2,1,2
+= 5
+
+
+
+1,2,3,4,5,4,3,2,1
+0,1,2,3,4,0,0,0,0
+0,0,0,0,4,3,2,1,0
+1,1,1,1,1,1,1,1,1
+1,2,3,4,9,4,3,2,1
+1,2,3,4,5,4,3,2,1
+
+1,2,3,9,8,7,6,5,4,3,2,1
+0,1,2,3,0,0,0,0,0,0,0,0
+0,0,0,8,7,6,5,4,3,2,1,0
+0,1,2,8,7,6,5,4,3,2,1,0
+
+1,2,3,4,5,2,1
+0,1,2,3,4,0,0
+0,1,2,3,4,1,0
+
+
+1,2,3,4,5,2,1
+0,1,2,3,4,1,0 
+
+time: O(2n) = O(n)
+space: O(2n)
+
+1,3,2,2,1
+0,1,0,0,0
+
+0,1,0,1,0
+1,1,1,1,1
+1,2,1,2,1
+ */
+
+function candy(ratings: number[]): number {
+    const RSize = ratings.length;
+    const extraCandy = Array(RSize).fill(0);
+    for (let i = 1; i < RSize; i++) {
+        if (ratings[i] > ratings[i-1]) {
+            extraCandy[i] = extraCandy[i-1]+ 1;
+        }
+    }
+    let totalCandyCount = extraCandy[RSize-1] + RSize;
+    for (let i = RSize-2; i > -1; i--) {
+        if (ratings[i] > (ratings[i+1] || 0)) {
+            extraCandy[i] = Math.max(extraCandy[i], extraCandy[i+1] + 1);
+        }
+        totalCandyCount += extraCandy[i];
+    }
+    return totalCandyCount;
+};
