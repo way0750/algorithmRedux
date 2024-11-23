352c056c75194e5f0792ae7bf2c09f234f37e620
Huiqiang Huang <bruce0750@gmail.com>
2024-11-22 21:11:43 -0800

    new chalenge

--- algorithm/Trapping Rain Water.ts
+++ algorithm/Trapping Rain Water.ts
@@ -17,4 +17,88 @@ Constraints:
 n == height.length
 1 <= n <= 2 * 104
 0 <= height[i] <= 105
+const strToNum = {
+    "I": 1,
+    "II": 2,
+    "III": 3,
+    "IV": 4,
+    "V": 5,
+    "VI": 6,
+    "VII": 7,
+    "VIII": 8,
+    "IX": 9,
+
+    "X": 10,
+    "XX": 20,
+    "XXX": 30,
+    "XL": 40,
+    "L": 50,
+    "LX": 60,
+    "LXX": 70,
+    "LXXX": 80,
+    "XC": 90,
+
+    "C": 100,
+    "CC": 200,
+    "CCC": 300,
+    "CD": 400,
+    "D": 500,
+    "DC": 600,
+    "DCC": 700,
+    "DCCC": 800,
+    "CM": 900,
+
+    "M": 1000,
+    "MM": 2000,
+    "MMM": 3000,
+};
  */
+
+
+const strToNum = {
+    "I": 1,
+    "II": 2,
+    "III": 3,
+    "IV": 4,
+    "V": 5,
+    "VI": 6,
+    "VII": 7,
+    "VIII": 8,
+    "IX": 9,
+
+    "X": 10,
+    "XX": 20,
+    "XXX": 30,
+    "XL": 40,
+    "L": 50,
+    "LX": 60,
+    "LXX": 70,
+    "LXXX": 80,
+    "XC": 90,
+
+    "C": 100,
+    "CC": 200,
+    "CCC": 300,
+    "CD": 400,
+    "D": 500,
+    "DC": 600,
+    "DCC": 700,
+    "DCCC": 800,
+    "CM": 900,
+
+    "M": 1000,
+    "MM": 2000,
+    "MMM": 3000,
+};
+
+function romanToInt(s: string): number {
+    let i = 0;
+    let num = 0;
+    while (i < s.length) {
+        let sLength = 4;
+        while (!strToNum[s.slice(i, i + sLength)]) sLength--;
+        num += strToNum[s.slice(i, i + sLength)];
+        i += sLength
+    }
+    return num;
+};
