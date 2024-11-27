df9715a3e2fad3790f74297c4c4da25cde75f115
Huiqiang Huang <bruce0750@gmail.com>
2024-11-26 17:13:27 -0800

    solution

--- algorithm/15. 3Sum.ts
+++ algorithm/15. 3Sum.ts
@@ -32,3 +32,36 @@ Constraints:
 3 <= nums.length <= 3000
 -105 <= nums[i] <= 105
  */
+
+/**
+0,0,0,0,0,0,0
+[0,0,0]
+-1,-1,-1,0,0,0,1,1,1
+-1, -1, 1
+0, 0, 0
+-3,-3,-3,-2,-2,-2,-1,-1,-1,0,0,0,1,1,1,2,2,2,3,3,3
+
+ */
+function threeSum(nums: number[]): number[][] {
+    nums = nums.sort((a, b) => a-b);
+    const ans = [];
+    for (let i = 0; i < nums.length-2; i++) {
+        if (nums[i] !== nums[i-1]) {
+            let left = i+1;
+            let right = nums.length-1;
+            while (left < right) {
+                const sum = nums[i] + nums[left] + nums[right];
+                if (sum === 0) {
+                    ans.push([nums[i], nums[left], nums[right]]);
+                    left++;
+                    while (nums[left] === nums[left-1]) left++;
+                } else if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                }
+            }
+        }
+    }
+    return ans;
+};
