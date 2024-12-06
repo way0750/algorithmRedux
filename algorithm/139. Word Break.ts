f626ffa5c6a967b6a7d40ecf3f451c1783182b0c
Huiqiang Huang <bruce0750@gmail.com>
2024-12-05 19:36:40 -0800

    solution

--- algorithm/139. Word Break.ts
+++ algorithm/139. Word Break.ts
@@ -29,4 +29,47 @@ Constraints:
 1 <= wordDict[i].length <= 20
 s and wordDict[i] consist of only lowercase English letters.
 All the strings of wordDict are unique.
+
+build from small to larger cases
+""
+"l"
+"le"
+"lee"
+"leet"
+
+for each case, go from the last char in that case and accumlate chars backward
+    and each time, check it against the dict see if that chunck of chars exist in the dict
+    anytime that's a yes, add to cache[case] = true;
+
+    how far to "look back"? as far as the longest word in the dict
+
+return cache[input str]
+time: O(s*c)
+space: O(s**2)
  */
+
+function wordBreak(s: string, wordDict: string[]): boolean {
+    let bigCharLen = 0;
+    const words = {};
+    for (let i = 0; i < wordDict.length; i++) {
+        bigCharLen = Math.max(bigCharLen, wordDict[i].length);
+        words[wordDict[i]] = true;
+    }
+
+    const cache = {'': true};
+    for (let i = 0; i < s.length; i++) {
+        let j = i;
+        let loopBackCount = bigCharLen;
+        const sCase = s.slice(0, i+1);
+        cache[sCase] = false;
+        while (loopBackCount && !cache[sCase]) {
+            const chunck = s.slice(j, i+1);
+            if (chunck in words && cache[s.slice(0, j)]) {
+                cache[sCase] = true;
+            }
+            loopBackCount--;
+            j--;
+        }
+    }
+    return cache[s];
+};
