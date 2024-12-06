0f9c172b902284826e308b5a02849b56d29b45a4
Huiqiang Huang <bruce0750@gmail.com>
2024-12-05 23:53:28 -0800

    challenge

--- algorithm/50. Pow(x, n).ts
+++ algorithm/50. Pow(x, n).ts
@@ -1,28 +1,28 @@
 /**
- * Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+ * Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
  
 
 Example 1:
 
-
-Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
-Output: 4
+Input: x = 2.00000, n = 10
+Output: 1024.00000
 Example 2:
 
-
-Input: matrix = [["0","1"],["1","0"]]
-Output: 1
+Input: x = 2.10000, n = 3
+Output: 9.26100
 Example 3:
 
-Input: matrix = [["0"]]
-Output: 0
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
  
 
 Constraints:
 
-m == matrix.length
-n == matrix[i].length
-1 <= m, n <= 300
-matrix[i][j] is '0' or '1'.
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+n is an integer.
+Either x is not zero or n > 0.
+-104 <= xn <= 104
  */
