3b21d31e1997c0f09128b46e75711bc4b3ec5438
Huiqiang Huang <bruce0750@gmail.com>
2024-12-07 02:40:07 -0800

    new

---
+++ algorithm/257. Binary Tree Paths.ts
@@ -0,0 +1,48 @@
+/**
+ * Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+Example 2:
+
+Input: root = [1]
+Output: ["1"]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 100].
+-100 <= Node.val <= 100
+ */
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function binaryTreePaths(root: TreeNode | null): string[] {
+    if (!root) return [];
+    if (!root.left && !root.right) return [`${root.val}`];
+    const left = binaryTreePaths(root.left);
+    const right = binaryTreePaths(root.right);
+    const ans = [...left, ...right];
+    return ans.map((path) => {
+        return `${root.val}->` + path;
+    });
+};
