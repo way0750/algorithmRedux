9e2704f7251c5b878452cbd5c8ddc21e4a8b9d25
Huiqiang Huang <bruce0750@gmail.com>
2024-12-15 02:08:07 -0800

    solution

--- algorithm/22. Generate Parentheses.ts
+++ algorithm/22. Generate Parentheses.ts
@@ -18,3 +18,41 @@ Constraints:
 1 <= n <= 8
 
  */
+
+function generateParenthesis(n: number): string[] {
+    let pairs = ['()'];
+    for (let i = 2; i <= n; i++) {
+        const newPairs = [];
+        for (let j = 0; j < pairs.length; j++) {
+            const pair = pairs[j];
+            let pCount = 0;
+            for (let k = -1; k < pair.length; k++) {
+                const char = pair[k];
+                if (char === '(') {
+                    pCount++;
+                } else if (char === ')') {
+                    pCount--
+                }
+                if (pCount === 0) {
+                    newPairs.push(
+                        '(' + pair.substr(0, k+1)  + ')' + pair.slice(k+1)
+                    );
+                }
+            }
+        }
+        pairs = newPairs;
+    }
+
+    return pairs;
+};
+/**
+1: ()
+2: a: ()() b:(())
+3: a: ()()(), (())(), (()())
+   b: ()(()), ((()))
+
+1: ()
+loop through all previous pairs
+    count opening and closings
+        whenever count === 0, add ) there
+ */
