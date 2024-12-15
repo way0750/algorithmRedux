37c9f0f54c285deb7045c1c0c84ffe58a2de3fc1
Huiqiang Huang <bruce0750@gmail.com>
2024-12-15 00:28:15 -0800

    solution

--- algorithm/96. Unique Binary Search Trees.ts
+++ algorithm/96. Unique Binary Search Trees.ts
@@ -18,3 +18,16 @@ Constraints:
 
 1 <= n <= 19
  */
+
+function numTrees(n: number): number {
+    const cache = { 0: 1, 1: 1, 2: 2, 3: 5 };
+    if (cache[n]) return cache[n];
+    for (let i = 4; i <= n; i++) {
+        cache[i] = 0;
+        for (let left = 0; left < i; left++) {
+            const right = i - left - 1;
+            cache[i] += cache[left] * cache[right];
+        }
+    }
+    return cache[n];
+};
