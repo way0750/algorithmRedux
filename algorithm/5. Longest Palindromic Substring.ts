b2e1ae25d35a0292ca8a17bd352c8e335f9d4ccd
Huiqiang Huang <bruce0750@gmail.com>
2025-02-13 15:40:52 -0800

    done

--- algorithm/5. Longest Palindromic Substring.ts
+++ algorithm/5. Longest Palindromic Substring.ts
@@ -23,3 +23,35 @@ Constraints:
 1 <= s.length <= 1000
 s consist of only digits and English letters.
  */
+
+const longestPalindrome = function(s) {
+    const ls = '#' + s.replace(/\w/g, (w) => `${w}#`);
+    const dp = Array(ls.length).fill(0);
+    let centerI = 0;
+    let rightReach = 0;
+    for (let i = 0; i < ls.length; i++) {
+        if (i < rightReach) {
+            dp[i] = Math.min(rightReach - i, dp[2*centerI - i]);
+        }
+
+        while (ls[i-dp[i]-1] && ls[i-dp[i]-1] === ls[i+dp[i]+1]) {
+            dp[i]++;
+        }
+        if (i+dp[i] > rightReach) {
+            centerI = i;
+            rightReach = i + dp[i];
+        }
+    }
+    let maxR = -1;
+    let maxRI = -1;
+    for (let i = 0; i < dp.length; i++) {
+        if (dp[i] > maxR) {
+            maxR = dp[i];
+            maxRI = i;
+        }
+    }
+
+    const start = maxRI - maxR;
+    const end = maxRI + maxR;
+    return ls.substring(start, end).replace(/#/g, '');
+};
