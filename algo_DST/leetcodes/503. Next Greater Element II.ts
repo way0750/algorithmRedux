058a814ba049718fcc46d48ab49f7d40560275ca
Huiqiang Huang <bruce0750@gmail.com>
2025-02-17 20:41:44 -0800

    solution 003

--- algo_DST/leetcodes/503. Next Greater Element II.ts
+++ algo_DST/leetcodes/503. Next Greater Element II.ts
@@ -23,3 +23,52 @@ Constraints:
 1 <= nums.length <= 104
 -109 <= nums[i] <= 109
  */
+
+
+/**
+ 0. 1. 2  3. 4
+[1, 2, 3, 4, 3]
+
+  0.  1.  2.  3.  4
+[2, -1, -1, -1, -1]
+
+length = 5
+stack: a monotonic decreasing or === stack
+[4, 3]
+
+the stack of indexes of numbers that still haven't found their larger
+numbers yet, and the stack is sort in equal and descending order
+the loop:
+    for each index, the purpose is to assign itself as the next large number
+    for all previous number who still haven't found one yet
+        and you can tell which numbers still the current num because the stack
+            is sorted in equal / descending order so just compare those numbers
+            to self until getting one that's larger
+    it just needs to go twice, in which case, that will be able to
+find larger number for any index (even the last index)
+
+time:
+o(n * 2 + n for the stack)
+no you won't get n**2 because you will put all numbers in it at most
+2 twice and pop at most twice in the entire run
+so O(n*2 + n*2) = O(n)
+space: O(n) for the monotonic stack
+
+what is the essence of monotonic stack and application?
+keepping prevous unprocessed data until later point?
+that's just stack in general
+by having order then what?
+you are only choosing some of data in the stack
+ */
+var nextGreaterElements = function(nums) {
+    const waitIs = [];
+    const ans = Array(nums.length).fill(-1);
+    for (let i = 0; i < (nums.length * 2); i++) {
+        const realI = i % nums.length;
+        while (waitIs.length && nums[waitIs[waitIs.length-1]] < nums[realI]) {
+            ans[waitIs.pop()] = nums[realI];
+        }
+        waitIs.push(realI);
+    }
+    return ans;
+};
