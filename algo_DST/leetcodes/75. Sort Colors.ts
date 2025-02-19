93ba76171f55128c9d310aebd5ff1ebccfd8822a
Huiqiang Huang <bruce0750@gmail.com>
2025-02-18 18:35:00 -0800

    challenge 003

--- algo_DST/leetcodes/75. Sort Colors.ts
+++ algo_DST/leetcodes/75. Sort Colors.ts
@@ -26,3 +26,51 @@ nums[i] is either 0, 1, or 2.
 
 Follow up: Could you come up with a one-pass algorithm using only constant extra space?
  */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+           |
+ 0,0,0,1,1,2,2,2,2
+           |
+      |
+0,0,1,1,2,2
+      |
+        ^
+
+2,0,2,1,1,0
+
+    |
+0,0,1,1,2,2
+      |
+    |
+set two pointers
+zeroP and twoP
+pointer at the spot where swap can happen for 0 or 2
+then loop through the array using while loop
+    because after swapping the current point needs to stay
+        and see if there is more swapping to do
+    loop while current pointer is <= twoP
+after each swapping more respective pointer to right or left
+time: O(n) because worst case we need to do 2 swap at each index
+so 2n = n
+space; O(1) it's done in place
+ */
+var sortColors = function(nums) {
+    let zeroP = 0;
+    let twoP = nums.length - 1;
+    let curP = 0;
+    while (curP <= twoP) {
+        const num = nums[curP];
+        if (num === 0) {
+            [nums[curP], nums[zeroP]] = [nums[zeroP], nums[curP]];
+            curP++;
+            zeroP++;
+        } else if (num === 2) {
+            [nums[curP], nums[twoP]] = [nums[twoP], nums[curP]];
+            twoP--;
+        } else {
+            curP++;
+        }
+    }
+};
