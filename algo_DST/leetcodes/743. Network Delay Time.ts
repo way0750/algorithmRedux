97859cb1bbda71d07ccedfb148876d38a16294f6
Huiqiang Huang <bruce0750@gmail.com>
2025-02-24 23:09:36 -0800

    solution 3

--- algo_DST/leetcodes/743. Network Delay Time.ts
+++ algo_DST/leetcodes/743. Network Delay Time.ts
@@ -30,3 +30,54 @@ ui != vi
 0 <= wi <= 100
 All the pairs (ui, vi) are unique. (i.e., no multiple edges.)
  */
+
+
+
+var networkDelayTime001 = function(times, n, k) {
+    // {from : [[to, weight]], [to, weight]};
+    const graph = Array(n+1).fill(1).map(() => []);
+
+    times.forEach(([from, to, weight]) => {
+        graph[from].push([to, weight]);
+    });
+
+    const distRecord = Array(n+1).fill(Infinity);
+    const queue = [[k, 0]]; // [[node, accumulative weight]]
+    while (queue.length) {
+        queue.sort((a, b) => a[1] - b[1]);
+        const [currentNode, totalWeight] = queue.shift();
+        if (totalWeight < distRecord[currentNode]) {
+            distRecord[currentNode] = totalWeight;
+            const neighbors = graph[currentNode] || [];
+            neighbors.forEach((neighbor) => {
+                queue.push([neighbor[0], neighbor[1] + totalWeight]);
+            });
+        }
+    }
+    distRecord.shift() // get rid of the first Infinity
+    let max = -1;
+    for (let i = 0; i < distRecord.length; i++) {
+        if (distRecord[i] === Infinity) return -1;
+        max = Math.max(max, distRecord[i]);
+    }
+
+    return max;
+};
+/**
+
+graph: [
+    0: []
+    1: [[2, 1], [3, 1]]
+    2: [[3, 2]]
+    3: []
+    distRecord: [
+        0: Infinity
+        1: Infinity
+        2: 0
+        3: Infinity
+    ]
+
+    q [[2, 0]]
+]
+
+ */
