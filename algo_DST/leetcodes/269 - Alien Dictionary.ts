72af00d90dfbe813f104a4ffecb90aecd66df3e7
Huiqiang Huang <bruce0750@gmail.com>
2025-03-05 17:10:47 -0800

    challenge 1

---
+++ algo_DST/leetcodes/269 - Alien Dictionary.ts
@@ -0,0 +1,38 @@
+/**
+ * 269 - Alien Dictionary
+Posted on August 25, 2016
+11 minute read
+Welcome to Subscribe On Youtube
+
+Question
+Formatted question description: https://leetcode.ca/all/269.html
+
+There is a new alien language that uses the English alphabet. However, the order among the letters is unknown to you.
+
+You are given a list of strings words from the alien language's dictionary, where the strings in words are sorted lexicographically by the rules of this new language.
+
+Return a string of the unique letters in the new alien language sorted in lexicographically increasing order by the new language's rules. If there is no solution, return "". If there are multiple solutions, return any of them.
+
+ 
+
+Example 1:
+
+Input: words = ["wrt","wrf","er","ett","rftt"]
+Output: "wertf"
+Example 2:
+
+Input: words = ["z","x"]
+Output: "zx"
+Example 3:
+
+Input: words = ["z","x","z"]
+Output: ""
+Explanation: The order is invalid, so return "".
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of only lowercase English letters.
+ */
