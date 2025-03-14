77bd5d9d47d0f45996f14dbbc4569443e844dd7d
Huiqiang Huang <bruce0750@gmail.com>
2025-03-13 23:06:12 -0700

    solution 3

--- algo_DST/impressive.md
+++ algo_DST/impressive.md
@@ -4,3 +4,6 @@ https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parenthese
 # use a heap of certain size as data coming in
     https://leetcode.com/problems/k-closest-points-to-origin/solutions/220235/java-three-solutions-to-this-classical-k-th-problem/
     doing so will always give you the min/max range of the data stream quickly
+
+# again, just map out all the possible solutions:
+    https://leetcode.com/problems/next-greater-numerically-balanced-number/solutions/1537485/fastest-code-so-far/
---
+++ algo_DST/leetcodes/17. Letter Combinations of a Phone Number.ts
@@ -0,0 +1,34 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const dToS = {
+    1: [''],
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+    0: [''],
+};
+
+var letterCombinations = function(digits, idx) {
+    const ans = []
+    if (!digits) return ans;
+    const dfs = (idx, str) => {
+        if (idx === digits.length) {
+            ans.push(str);
+            return;
+        }
+        const choices = dToS[digits[idx]];
+        for (let choice of choices) {
+            const newStr = str + choice;
+            dfs(idx+1, newStr);
+        }
+    }
+    dfs(0, '');
+    return ans;
+};
