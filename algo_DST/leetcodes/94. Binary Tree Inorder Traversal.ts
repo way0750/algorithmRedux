04ca5646498e7febc9bcd2cd739da33db09ab13e
Huiqiang Huang <bruce0750@gmail.com>
2025-04-27 17:01:38 -0700

    solution 5

---
+++ algo_DST/leetcodes/94. Binary Tree Inorder Traversal.ts
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root, path = []) {
+    if (!root) return path;
+    inorderTraversal(root.left, path);
+    path.push(root.val);
+    inorderTraversal(root.right, path);
+    return path;
+};
