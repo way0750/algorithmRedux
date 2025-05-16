549c8007def7c0a26fdab1b5b25511d0854ee1e9
Huiqiang Huang <bruce0750@gmail.com>
2025-05-16 14:03:56 -0700

    solution 2

---
+++ algo_DST/leetcodes/2469. Convert the Temperature.ts
@@ -0,0 +1,10 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    return [
+        celsius + 273.15,
+        celsius * 1.8 + 32,
+    ];
+};
